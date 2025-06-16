e8a628cb807aaf9091ef52c3f665946fd82e8b2e
SHIVAM OJHA <156840180+ojha-jee@users.noreply.github.com>
2025-06-16 12:22:51 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Event_Management_System
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            SeedAdminAndRoles(); 
+        }
+
+        private void SeedAdminAndRoles()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+             
+                if (!roleManager.RoleExists("Admin"))
+                    roleManager.Create(new IdentityRole("Admin"));
+
+                if (!roleManager.RoleExists("User"))
+                    roleManager.Create(new IdentityRole("User"));
+
+           
+                var adminEmail = "ojha0205@gmail.com";
+                var adminPassword = "Admin@123";
+
+                
+                var existingAdmin = userManager.FindByEmail(adminEmail);
+                if (existingAdmin != null)
+                {
+                    
+                    var roles = userManager.GetRoles(existingAdmin.Id);
+                    foreach (var role in roles)
+                    {
+                        userManager.RemoveFromRole(existingAdmin.Id, role);
+                    }
+
+                    
+                    userManager.Delete(existingAdmin);
+                }
+
+             
+                var newAdmin = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FirstName = "Admin",
+                    LastName = "User",
+                    Age = 30,
+                    ContactNumber = "1234567890",
+                    EmailConfirmed = true 
+                };
+
+                var createResult = userManager.Create(newAdmin, adminPassword);
+                if (createResult.Succeeded)
+                {
+                    userManager.AddToRole(newAdmin.Id, "Admin");
+                }
+                else
+                {
+                    throw new Exception("Failed to create admin: " + string.Join("; ", createResult.Errors));
+                }
+
+            }
+            
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(Event_Management_System.Startup))]
+
+namespace Event_Management_System
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+        }
+    }
+}
